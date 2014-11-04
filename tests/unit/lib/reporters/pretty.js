58ff8704633ac956ef1e800c133f23857a16adf9
devpaul <pshannon@sitepen.com>
2014-11-03 18:41:09 -0700

    Added drawing of a reporter

--- tests/unit/lib/reporters/pretty.js
+++ tests/unit/lib/reporters/pretty.js
@@ -16,11 +16,12 @@ define([
 					}
 				};
 				mockCharm.out = '';
+				pretty.charm = mockCharm;
 			},
 
 			'_Report': function () {
-				var report = new pretty._Report('type');
-				assert.deepEqual(report.type, 'type');
+				var report = new pretty._Report();
+				assert.deepEqual(report.environment, {});
 				report.record(0);
 				report.record(1);
 				report.record(2);
@@ -35,21 +36,44 @@ define([
 				assert.deepEqual(report.getCompressedResults(4), [0, 1, 2]);
 			},
 
-			'_drawProgressBar': function () {
-				var report = new pretty._Report('test');
+			'_drawProgressBar': (function () {
+				var report;
+
+				return {
+					beforeEach: function () {
+						report = new pretty._Report('test');
+						report.record(0);
+						report.record(1);
+						report.record(2);
+						report.record(1);
+						report.record(0);
+					},
+
+					large: function () {
+						report.total = 100;
+						pretty._drawProgressBar(report, 20, mockCharm);
+						assert.equal(mockCharm.out, '[×         ]   5/100');
+					},
+
+					small: function () {
+						report.total = 10;
+						pretty._drawProgressBar(report, 20, mockCharm);
+						assert.equal(mockCharm.out, '[✓~×~✓     ]  5/10');
+					}
+				};
+			})(),
+
+			'_drawReporter': function () {
+				var report = new pretty._Report({
+					browserName: 'chrome',
+					version: '32.0.12'
+				});
 				report.record(0);
 				report.record(1);
 				report.record(2);
-				report.record(1);
-				report.record(0);
 				report.total = 10;
-				pretty._drawProgressBar(report, 20, mockCharm);
-				assert.equal(mockCharm.out, '[✓~×~✓       ]  5/10');
-
-				report.total = 100;
-				mockCharm.out = '';
-				pretty._drawProgressBar(report, 20, mockCharm);
-				assert.equal(mockCharm.out, '[×         ]   5/100');
+				pretty._drawReporter(report, 80);
+				assert.equal(mockCharm.out, 'Chr 32:   [✓~×       ]  3/10, 1 fail, 1 skip');
 			}
 		};
 	});
